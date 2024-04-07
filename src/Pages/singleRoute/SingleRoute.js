4ee05c92101274a6eb6b1101b175601cab76d0ce
yahyojondev <nabijonovdeveloper@gmail.com>
2024-04-07 22:51:30 +0500

    hardwork

--- src/Pages/singleRoute/SingleRoute.js
+++ src/Pages/singleRoute/SingleRoute.js
@@ -1,5 +1,5 @@
 import React, { memo, useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, NavLink } from "react-router-dom";
 import axios from "axios";
 import { IoStarSharp } from "react-icons/io5";
 import { AiOutlineUp, AiOutlineDown } from "react-icons/ai";
@@ -11,7 +11,7 @@ let API_URL = "https://fakestoreapi.com/products";
 function SingleRoute() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-  const [count, setcount] = useState(0);
+  const [count, setcount] = useState(1);
   const [loading, setLoading] = useState(false);
   const [price, setPrice] = useState(false);
 
@@ -71,14 +71,17 @@ function SingleRoute() {
                 <div className="buy__wrapper">
                   <div className="count__wrapper">
                     <span>{count}</span>
-                    <div className="nav__images">
+                    <button className="nav__images">
                       <div onClick={() => setcount((p) => p + 1)}>
                         <AiOutlineUp />
                       </div>
-                      <div onClick={() => setcount((p) => p - 1)}>
+                      <button
+                        disabled={count === 1}
+                        onClick={() => setcount((p) => p - 1)}
+                      >
                         <AiOutlineDown x />
-                      </div>
-                    </div>
+                      </button>
+                    </button>
                   </div>
                   <button className="single__btn__add">
                     <IoCartOutline />
@@ -89,6 +92,57 @@ function SingleRoute() {
                   </div>
                 </div>
               </div>
+            </div>
+            <div className="singleRoute__wrapper__left__bottom">
+              <ul className="singleRoute__wrapper__left__bottom__list">
+                <li className="singleRoute__wrapper__left__bottom__list__item">
+                  <NavLink></NavLink>
+                </li>
+              </ul>
+              <p className="singleRoute__wrapper__bottom__text">
+                Uninhibited carnally hired played in whimpered dear gorilla
+                koala depending and much yikes off far quetzal goodness and from
+                for grimaced goodness unaccountably and meadowlark near
+                unblushingly crucial scallop tightly neurotic hungrily some and
+                dear furiously this apart. Spluttered narrowly yikes left moth
+                in yikes bowed this that grizzly much hello on spoon-fed that
+                alas rethought much decently richly and wow against the frequent
+                fluidly at formidable acceptably flapped besides and much circa
+                far over the bucolically hey precarious goldfinch mastodon
+                goodness gnashed a jellyfish and one however because.
+              </p>
+              <ul className="single__wrapper__bottom__list">
+                <li className="single__wrapper___bottom__item"></li>
+              </ul>
+              <hr />
+              <p className="singleRoute__wrapper__bottom__text">
+                Laconic overheard dear woodchuck wow this outrageously taut
+                beaver hey hello far meadowlark imitatively egregiously hugged
+                that yikes minimally unanimous pouted flirtatiously as beaver
+                beheld above forward energetic across this jeepers beneficently
+                cockily less a the raucously that magic upheld far so the this
+                where crud then below after jeez enchanting drunkenly more much
+                wow callously irrespective limpet.
+              </p>
+              <h2 className="singleRoute__wrapper__bottom__title">
+                Packaging & Delivery
+              </h2>
+              <hr />
+              <p className="singleRoute__wrapper__bottom__text">
+                Less lion goodness that euphemistically robin expeditiously
+                bluebird smugly scratched far while thus cackled sheepishly
+                rigid after due one assenting regarding censorious while
+                occasional or this more crane went more as this less much amid
+                overhung anathematic because much held one exuberantly sheep
+                goodness so where rat wry well concomitantly. Scallop or far
+                crud plain remarkably far by thus far iguana lewd precociously
+                and and less rattlesnake contrary caustic wow this near alas and
+                next and pled the yikes articulate about as less cackled
+                dalmatian in much less well jeering for the thanks blindly
+                sentimental whimpered less across objectively fanciful grimaced
+                wildly some wow and rose jeepers outgrew lugubrious luridly
+                irrationally attractively dachshund.
+              </p>
             </div>
           </div>
           <div className="singleRoute__wrapper__right"></div>
